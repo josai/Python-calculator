95d684e08f896bcd55f7145febbf18407fcc400c
Shahneela79 <31089157+Shahneela79@users.noreply.github.com>
2017-08-27 22:37:55 +0500

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,42 +1,58 @@
-#This is a change on line 1 
-run = True 
+def add(operand1, operand2):
+   return operand1 + operand2
 
-while run:
-    print("1 = Addition")
-    print("2 = Subtraction")
-    print("3 = Multiplication")
-    print("4 = Division")
-    print("5 = Exit program")
-    cal = int(input("Enter number : "))
-    if cal == 1:
-        print("Addition")
-        first = int(input("Enter first number :"))
-        secund = int(input("Enter secund number :"))
-        result = first + secund
-        print("result =" , result)
-    elif cal == 2:
-        print("Subtraction")
-        first = int(input("Enter first number :"))
-        secund = int(input("Enter secund number :"))
-        result = first - secund
-        print("result =" , result)
-    elif cal == 3:
-        print("Mmltiplication")
-        first = int(input("Enter first number :"))
-        secund = int(input("Enter secund number :"))
-        result = first * secund
-        print("result =" , result)
-    elif cal == 4:
-        print("Division")
-        one = int(input("Enter first number :"))
-        two = int(input("Enter secund number :"))
-        if two <1:
+def subtract(operand1, operand2):
+   return operand1 - operand2
+
+def multiply(operand1, operand2):
+   return operand1 * operand2
+
+def divide(operand1, operand2):
+   return operand1 / operand2
+   
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Exit")
+
+choice = int(input("Enter choice(1, 2, 3, 4, 5):"))
+if choice == 5:
+  print("goodbye")
+  exit()
+elif choice >= 6:
+  print("invalid number")
+  exit()
+
+operand1 = (input("Enter first number: "))
+operand2 = (input("Enter Second number: "))
+
+if type(input) == "str":
+    print ("invalid input")
+    eixt()
+elif type(input) == "int" or type (input) =="float": 
+    print ("valid")
+
+if choice == 1:
+   print(operand1,"+",operand2,"=", add(operand1,operand2))
+
+elif choice == 2:
+   print(operand1,"-",operand2,"=", subtract(operand1,operand2))
+
+elif choice == 3:
+   print(operand1,"*",operand2,"=", multiply(operand1,operand2))
+
+elif choice == 4:
+  if operand2 <1:
           print("Sorry!no value can be divided by zero ")
-        elif two>1:
-          result = one / two
-          print("result =" , result)
-    elif cal == 5:
-        print("Quit!")
-        run = False
+  elif operand2>1:
+   print(operand1,"/",operand2,"=", divide(operand1,operand2))
 
+elif choice == 5:
+   print("Goodbye")
 
+else:
+  print("invalid input")
+  exit()
+   
